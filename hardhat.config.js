86b0ef4a7ff0f046240dc991c265c4574400b564
nubis <nubis@constata.eu>
2026-06-28 16:18:18 +0200

    adds hardhat config

---
+++ hardhat.config.js
@@ -0,0 +1,25 @@
+import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
+import { defineConfig } from "hardhat/config";
+export default defineConfig({
+    plugins: [hardhatToolboxViemPlugin],
+    solidity: {
+        version: "0.8.24",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200,
+            },
+        },
+        npmFilesToBuild: [
+            "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol",
+            "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol",
+            "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol",
+        ],
+    },
+    paths: {
+        sources: "test/contracts",
+        artifacts: "artifacts",
+        cache: "cache",
+    },
+});
+//# sourceMappingURL=hardhat.config.js.map
